421e0c252f018a2b2d596cd5b428cd9277e1cf2e
Saksham Mittal <gotlouemail@gmail.com>
2023-02-27 20:07:06 +0530

    Use arti_hyper as well

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,23 @@
 use arti_client::*;
+use arti_hyper::*;
+
+use anyhow::Result;
+use arti_client::{TorClient, TorClientConfig};
+use hyper::Body;
+use tls_api::{TlsConnector as TlsConnectorTrait, TlsConnectorBuilder};
+
+// On apple-darwin targets there is an issue with the native and rustls
+// tls implementation so this makes it fall back to the openssl variant.
+//
+// https://gitlab.torproject.org/tpo/core/arti/-/issues/715
+#[cfg(not(target_vendor = "apple"))]
+use tls_api_native_tls::TlsConnector;
+#[cfg(target_vendor = "apple")]
+use tls_api_openssl::TlsConnector;
+
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
-async fn get_ip() {
+async fn get_ip_raw() {
     match arti_client::TorClient::builder().bootstrap_behavior(BootstrapBehavior::OnDemand).create_bootstrapped().await {
         Ok(client) => {
             let mut stream = client.connect(("icanhazip.com", 80)).await.unwrap();
@@ -28,9 +44,27 @@ async fn get_ip() {
     }
 }
 
+async fn get_ip_hyper() {
+    let config = TorClientConfig::default();
+    let tor_client = TorClient::create_bootstrapped(config).await.unwrap();
+    let tls_connector = TlsConnector::builder().unwrap().build().unwrap();
+
+    let connection = ArtiHttpConnector::new(tor_client, tls_connector);
+    let http = hyper::Client::builder().build::<_, Body>(connection);
+
+    eprintln!("requesting IP via Tor...");
+    let mut resp = http.get("https://icanhazip.com".try_into().unwrap()).await.unwrap();
+
+    eprintln!("status: {}", resp.status());
+
+    let body = hyper::body::to_bytes(resp.body_mut()).await.unwrap();
+    eprintln!("body: {}", std::str::from_utf8(&body).unwrap());
+}
+
 #[tokio::main]
 async fn main() {
     println!("Hello, world!");
-    get_ip().await;
-    get_ip().await;
+    get_ip_raw().await;
+    get_ip_hyper().await;
 }
+
