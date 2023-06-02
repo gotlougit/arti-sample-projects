98d19beb48fea27f40fcc4f70031e50aac8138ad
Saksham Mittal <gotlouemail@gmail.com>
2023-06-02 17:58:32 +0530

    Make OPCODES a bool array of size 4

--- crates/dns-resolver/src/main.rs
+++ crates/dns-resolver/src/main.rs
@@ -5,41 +5,37 @@ use serde::{Deserialize, Serialize};
 // header will be used by both types of messages so need to serialize and deserialize
 #[derive(Serialize, Deserialize)]
 struct Header {
-    identification: u16,
-    qr: bool, // set to 0 for requests, check for 1 in response
-    // set the following 4 fields to zero
-    opcode0: bool,
-    opcode1: bool,
-    opcode2: bool,
-    opcode3: bool,
-    aa: bool,
-    tc: bool,
-    rd: bool,
-    ra: bool,
-    zandrcord: u8, // set to 0
-    qdcount: u16,  // set to 1 since we have 1 question
-    ancount: u16,  // set to 0 since client doesn't have answers
-    nscount: u16,  // set to 0
-    arcount: u16,  // set to 0
+    pub identification: u16,
+    pub qr: bool,           // set to 0 for requests, check for 1 in response
+    pub opcodes: [bool; 4], // set all 4 to zero
+    pub aa: bool,
+    pub tc: bool,
+    pub rd: bool,
+    pub ra: bool,
+    pub zandrcord: u8, // set to 0
+    pub qdcount: u16,  // set to 1 since we have 1 question
+    pub ancount: u16,  // set to 0 since client doesn't have answers
+    pub nscount: u16,  // set to 0
+    pub arcount: u16,  // set to 0
 }
 
 #[derive(Serialize)]
 struct Query {
-    header: Header,
-    qname: Vec<u8>, // domain name
-    qtype: u16,     // set to 0x0001 for A records
-    qclass: u16,    // set to 1 for Internet addresses
+    pub header: Header,
+    pub qname: Vec<u8>, // domain name
+    pub qtype: u16,     // set to 0x0001 for A records
+    pub qclass: u16,    // set to 1 for Internet addresses
 }
 
 #[derive(Deserialize)]
 struct Response {
-    header: Header,
-    name: u16,      // same as in Query
-    restype: u16,   // same as in Query
-    class: u16,     // Same as in Query
-    ttl: u16,       // Number of seconds to cache the result
-    rdlength: u16,  // Length of RDATA
-    rdata: Vec<u8>, // IP address(es)
+    pub header: Header,
+    pub name: u16,      // same as in Query
+    pub restype: u16,   // same as in Query
+    pub class: u16,     // Same as in Query
+    pub ttl: u16,       // Number of seconds to cache the result
+    pub rdlength: u16,  // Length of RDATA
+    pub rdata: Vec<u8>, // IP address(es)
 }
 
 #[tokio::main]
