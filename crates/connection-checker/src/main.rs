7aee23b7202f6365e80cc21805c01d21f3dd3f60
Saksham Mittal <gotlouemail@gmail.com>
2023-06-08 17:26:24 +0530

    Print circuit info

--- crates/connection-checker/src/main.rs
+++ crates/connection-checker/src/main.rs
@@ -6,6 +6,7 @@ use tor_rtcompat::PreferredRuntime;
 use tracing::info;
 
 const URL: &str = "https://www.torproject.org";
+const HOST_PORT: &str = "torproject.org:80";
 
 async fn test_connection(tor_client: TorClient<PreferredRuntime>) {
     let tls_connector = TlsConnector::builder().unwrap().build().unwrap();
@@ -16,9 +17,19 @@ async fn test_connection(tor_client: TorClient<PreferredRuntime>) {
     println!("Status code: {}", status);
 }
 
+// Connect to a sample host and try to
+async fn get_circuit(tor_client: &TorClient<PreferredRuntime>) {
+    let stream = tor_client.connect(HOST_PORT).await.unwrap();
+    let circ = stream.circuit().path();
+    for node in circ {
+        println!("{}", node);
+    }
+}
+
 async fn test_normal_connection() {
     let config = TorClientConfig::default();
     let tor_client = TorClient::create_bootstrapped(config).await.unwrap();
+    get_circuit(&tor_client).await;
     test_connection(tor_client).await;
 }
 
