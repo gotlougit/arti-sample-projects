5ce28d5613f0b06c239ded2c408ea6807467f2c0
Saksham Mittal <gotlouemail@gmail.com>
2023-06-13 19:19:30 +0530

    Update Tor Browser download link

--- crates/download-manager/src/main.rs
+++ crates/download-manager/src/main.rs
@@ -13,7 +13,7 @@ use tracing::warn;
 const REQSIZE: u64 = 1024 * 1024;
 // TORURL is the particular Tor Browser Bundle URL
 const TORURL: &str =
-    "https://dist.torproject.org/torbrowser/12.0.4/tor-browser-linux64-12.0.4_ALL.tar.xz";
+    "https://dist.torproject.org/torbrowser/12.0.7/tor-browser-linux64-12.0.7_ALL.tar.xz";
 const TESTURL: &str = "https://www.gutenberg.org/files/2701/2701-0.txt";
 // Save the TBB with this filename
 const DOWNLOAD_FILE_NAME: &str = "download.tar.xz";
