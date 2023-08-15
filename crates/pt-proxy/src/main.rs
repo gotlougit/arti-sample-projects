6970b0e870de485863d3f3e223973d1195bbe043
Saksham Mittal <gotlouemail@gmail.com>
2023-08-15 11:40:47 +0530

    First fully functioning pt-proxy

--- crates/pt-proxy/src/main.rs
+++ crates/pt-proxy/src/main.rs
@@ -1,8 +1,9 @@
 use anyhow::Result;
 use fast_socks5::client::{self, Socks5Stream};
-use fast_socks5::server::{Config, Socks5Server};
+use fast_socks5::server::Socks5Server;
 use std::str::FromStr;
-use tokio::net::TcpStream;
+use tokio::io::AsyncWriteExt;
+use tokio::net::{TcpListener, TcpStream};
 use tokio::time::Duration;
 use tokio_stream::StreamExt;
 use tor_chanmgr::transport::proxied::{settings_to_protocol, Protocol};
@@ -18,6 +19,15 @@ const CLIENT_STATE_LOCATION: &str = "/tmp/arti-pt-client";
 #[derive(Debug, thiserror::Error)]
 #[error("Error while obtaining bridge line data")]
 struct BridgeLineParseError;
+
+#[derive(Clone)]
+struct ForwardingCreds {
+    username: String,
+    password: String,
+    forward_endpoint: String,
+    obfs4_server_ip: String,
+    obfs4_server_port: u16,
+}
 
 fn build_server_config(
     protocol: &str,
@@ -80,59 +90,53 @@ fn read_cert_info() -> Result<String> {
                 .find(|part| part.starts_with("iat-mode="))
                 .ok_or(BridgeLineParseError)?;
             let complete_config = format!("{};{}", cert, iat);
-            return Ok(complete_config);
+            Ok(complete_config)
         }
         Err(e) => Err(e.into()),
     }
 }
 
-// TODO: make `forward_creds` a proper struct
-async fn create_socks5_server(
-    endpoint: &str,
-    forward_creds: Option<(String, String, String, u16)>,
-) -> Result<()> {
-    let forward = forward_creds.is_some();
-    let config = match forward {
-        true => Config::default()
-            .set_skip_auth(true)
-            .set_execute_command(false)
-            .to_owned(),
-        false => Config::default(),
-    };
-    let mut listener = Socks5Server::bind(endpoint).await?;
-    listener.set_config(config);
-    if forward {
-        while let Some(Ok(mut socks_socket)) = listener.incoming().next().await {
-            let forward_cred_clone = forward_creds.clone();
-            tokio::spawn(async move {
-                let (username, password, client_endpoint, obfs4_server_port) =
-                    forward_cred_clone.unwrap();
-                let mut forward_client = connect_to_obfs4_client(
-                    &client_endpoint,
-                    &username,
-                    &password,
-                    "127.0.0.1",
-                    obfs4_server_port,
-                )
-                .await
-                .unwrap();
-
-                tokio::io::copy_bidirectional(&mut socks_socket, &mut forward_client)
+async fn run_forwarding_server(endpoint: &str, forward_creds: ForwardingCreds) -> Result<()> {
+    let listener = TcpListener::bind(endpoint).await?;
+    while let Ok((mut client, _)) = listener.accept().await {
+        let forward_creds_clone = forward_creds.clone();
+        tokio::spawn(async move {
+            if let Ok(mut relay_stream) = connect_to_obfs4_client(
+                &forward_creds_clone.forward_endpoint,
+                &forward_creds_clone.username,
+                &forward_creds_clone.password,
+                &forward_creds_clone.obfs4_server_ip,
+                forward_creds_clone.obfs4_server_port,
+            )
+            .await
+            {
+                if let Err(e) = tokio::io::copy_bidirectional(&mut client, &mut relay_stream).await
+                {
+                    eprintln!("{:#?}", e);
+                }
+            } else {
+                eprintln!("Couldn't connect to obfs4 client, is it running?");
+                client
+                    .write_all(&[5, 5, 0, 1, 0, 0, 0, 0, 0, 0])
                     .await
                     .unwrap();
-            });
-        }
-    } else {
-        tokio::spawn(async move {
-            while let Some(Ok(socks_socket)) = listener.incoming().next().await {
-                tokio::spawn(async move {
-                    if let Err(e) = socks_socket.upgrade_to_socks5().await {
-                        eprintln!("{:#?}", e);
-                    }
-                });
             }
         });
     }
+    Ok(())
+}
+
+async fn run_socks5_server(endpoint: &str) -> Result<()> {
+    let listener = Socks5Server::bind(endpoint).await?;
+    tokio::spawn(async move {
+        while let Some(Ok(socks_socket)) = listener.incoming().next().await {
+            tokio::spawn(async move {
+                if let Err(e) = socks_socket.upgrade_to_socks5().await {
+                    eprintln!("{:#?}", e);
+                }
+            });
+        }
+    });
     Ok(())
 }
 
@@ -150,8 +154,8 @@ async fn main() -> Result<()> {
     let final_socks5_endpoint = "127.0.0.1:9050";
 
     // server code
-    create_socks5_server(final_socks5_endpoint, None).await?;
-    let server_params = build_server_config("obfs4", &server_addr, &final_socks5_endpoint)?;
+    run_socks5_server(final_socks5_endpoint).await?;
+    let server_params = build_server_config("obfs4", &server_addr, final_socks5_endpoint)?;
 
     let cr_clone = cur_runtime.clone();
     let mut server_pt = PluggableTransport::new(
@@ -192,26 +196,24 @@ async fn main() -> Result<()> {
 
     let settings = settings_to_protocol(SocksVersion::V5, obfs4_server_conf)?;
     match settings {
-        Protocol::Socks(_, auth) => {
-            match auth {
-                SocksAuth::Username(raw_username, raw_password) => {
-                    let username = std::str::from_utf8(&raw_username)?;
-                    let password = match raw_password.is_empty() {
-                        true => "\0",
-                        false => std::str::from_utf8(&raw_password)?,
-                    };
-                    let creds = Some((
-                        username.to_string(),
-                        password.to_string(),
-                        client_endpoint,
-                        obfs4_server_port,
-                    ));
-                    // FIXME: final hop doesn't get socks5 traffic
-                    create_socks5_server(&entry_addr, creds).await?;
-                }
-                _ => eprintln!("Unable to get credentials for obfs4 client process!"),
+        Protocol::Socks(_, auth) => match auth {
+            SocksAuth::Username(raw_username, raw_password) => {
+                let username = std::str::from_utf8(&raw_username)?;
+                let password = match raw_password.is_empty() {
+                    true => "\0",
+                    false => std::str::from_utf8(&raw_password)?,
+                };
+                let creds = ForwardingCreds {
+                    username: username.to_string(),
+                    password: password.to_string(),
+                    forward_endpoint: client_endpoint,
+                    obfs4_server_ip: String::from("127.0.0.1"),
+                    obfs4_server_port,
+                };
+                run_forwarding_server(&entry_addr, creds).await?;
             }
-        }
+            _ => eprintln!("Unable to get credentials for obfs4 client process!"),
+        },
         _ => eprintln!("Unexpected protocol"),
     }
 
